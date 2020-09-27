b279505762d7a2407d94f97a216b872307988408
gourab-sinha <gourab19964u@gmail.com>
2020-09-28 00:11:44 +0530

    Student course enrollment option

--- courses/views.py
+++ courses/views.py
@@ -12,6 +12,7 @@ from django.forms.models import modelform_factory
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 from django.db.models import Count
 from django.views.generic.detail import DetailView
+from students.forms import CourseEnrollForm
 
 
 
@@ -175,4 +176,12 @@ class CourseListView(TemplateResponseMixin, View):
 class CourseDetailView(DetailView):
     model = Course
     template_name = 'courses/course/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['enroll_form'] = CourseEnrollForm(initial={'course': self.object})
+
+        return context
+
+
 
--- students/views.py
+++ students/views.py
@@ -2,6 +2,9 @@ from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, authenticate
+from django.views.generic.edit import FormView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import CourseEnrollForm
 
 
 class StudentRegistrationView(CreateView):
@@ -16,6 +19,20 @@ class StudentRegistrationView(CreateView):
         login(self.request, user)
 
         return result
+
+
+class StudentEnrollCourseView(LoginRequiredMixin, FormView):
+    course = None
+    form_class = CourseEnrollForm
+
+    def form_invalid(self, form):
+        self.course = form.cleaned_data['course']
+        self.course.students.add(self.request.user)
+
+        return super().form_invalid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('student_course_detail', args=[self.course.id])
 
 
 
